b5c2fbcb58dd0de006c5152d212a90360815d390
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-11-16 18:46:13 +0100

    milestone4 completa e aggiunta ricerca su invio

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,7 +10,6 @@ import { state } from '../state.js';
         },
         methods: {
             log(){
-                console.log(this.state.searchText)
                 this.state.url = `https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=${this.state.searchText}`
                 this.state.searchText = ""
 
@@ -30,7 +29,7 @@ import { state } from '../state.js';
     <div class="headerWrapper d-flex justify-content-between mx-5 my-4">
         <h1 class="title text-white">BoolFlix</h1>
         <div class="searchbar p-2">
-            <input type="text" placeholder="Search" v-model="state.searchText">
+            <input type="text" placeholder="Search" v-model="state.searchText" v-on:keydown.enter="log()">
             <button class="px-2" v-on:click="log()"><i class="fa-solid fa-magnifying-glass"></i></button>
         </div>
     </div>
